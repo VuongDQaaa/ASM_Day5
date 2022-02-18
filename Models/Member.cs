79db7aff1c5a36c21916c60690b80eb9ecf206a5
Vuong <vuongit2932000@gmail.com>
2022-02-18 15:46:08 +0700

    new update

--- Models/Member.cs
+++ Models/Member.cs
@@ -7,7 +7,7 @@ namespace ASM_Day5.Models
         public string LastName {get; set;}
         public Gender Gender {get; set;}
         public DateTime DateOfBirth {get; set;}
-        public string PhoneNum {get; set;}
+        public string PhoneNumber {get; set;}
         public string BirthPlace {get; set;}
         public bool IsGraduated {get; set;}
         public string FullName
